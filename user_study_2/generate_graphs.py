66fcfaee8cf398dcfdf188d8da058d902e48e5f9
Laura Stampf <github@isadora.de>
2024-03-21 16:49:39 +0100

    Modify: minor changes to size of graph elements

--- user_study_2/generate_graphs.py
+++ user_study_2/generate_graphs.py
@@ -2,17 +2,19 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-TITLE_SIZE = 20
-AXES_SIZE = 15
-LEGEND_SIZE = 13
 EXPERIENCE_COLOR = "blue"
 NO_EXPERIENCE_COLOR = "orange"
 
 def graphs_performance_per_task(dependent_var: str, filename: str):
+    TITLE_SIZE = 40
+    AXES_SIZE = 30
+    LEGEND_SIZE = 28
+    TICK_SIZE = 26
+
     # Sort and filter tasks
     tasks = data['Task'].unique()
 
-    fig, axs = plt.subplots(2, 2, figsize=(15, 10), sharey=True)
+    fig, axs = plt.subplots(2, 2, figsize=(15, 15), sharey=True)
 
     # Flatten the axs for easy iteration
     axs = axs.flatten()
@@ -21,27 +23,32 @@ def graphs_performance_per_task(dependent_var: str, filename: str):
         task_data = data[data['Task'] == task].sort_values(by='Participant')
         colors = task_data['Experience level'].apply(lambda x: EXPERIENCE_COLOR if x == "Some" else NO_EXPERIENCE_COLOR).to_list()
         
-        axs[i].bar(task_data['Participant'], task_data[dependent_var], color=colors, alpha=0.75)
+        axs[i].bar(task_data['Participant'], task_data[dependent_var], color=colors)
         axs[i].set_title(f'Task {task}', fontsize=TITLE_SIZE)
         axs[i].set_xlabel('Participant', fontsize=AXES_SIZE)
         axs[i].set_ylabel(dependent_var, fontsize=AXES_SIZE)
         axs[i].set_xticks(task_data['Participant'])
 
+        # Adjust tick labels size for each subplot
+        axs[i].tick_params(axis='x', labelsize=TICK_SIZE)
+        axs[i].tick_params(axis='y', labelsize=TICK_SIZE)
+
     # Adding a legend
-    labels = ['No Experience', 'Experienced']
+    labels = ['No Experience', 'Some Experience']
     handles = [plt.Rectangle((0,0),1,1, color=color, ec="k") for color in [NO_EXPERIENCE_COLOR,EXPERIENCE_COLOR]]
-    plt.figlegend(handles, labels, loc='upper right', borderaxespad=0.1, fontsize=LEGEND_SIZE)
+    plt.figlegend(handles, labels, loc=(0.67, 0.35), borderaxespad=0.1, fontsize=LEGEND_SIZE)
 
     plt.tight_layout()
     plt.savefig(filename, format='pdf', dpi=300, bbox_inches='tight')  # Save the figure
 
 def graph_average_performance_per_task(dependent_var: str, filename: str):
+    AXES_SIZE = 18
+    LEGEND_SIZE = 15
+    TICK_SIZE = 13
+
     # Splitting the dataframe based on experience level
     df_exp = data[data['Experience level'] == 'Some']
     df_no_exp = data[data['Experience level'] != 'Some']
-
-    print(df_exp)
-    print(df_no_exp)
 
     # Calculating averages and error margins for time taken
     time_avg_exp = df_exp.groupby('Task')[dependent_var].mean()
@@ -70,9 +77,11 @@ def graph_average_performance_per_task(dependent_var: str, filename: str):
     # Labels, title and legend
     ax.set_xlabel('Task Number', fontsize=AXES_SIZE)
     ax.set_ylabel(f'Average {dependent_var}', fontsize=AXES_SIZE)
-    # ax.set_title(f'Average {dependent_var} per Task by Experience Level', fontsize=TITLE_SIZE)
     ax.set_xticks(tasks)
     ax.legend(fontsize=LEGEND_SIZE)
+
+    plt.yticks(fontsize=TICK_SIZE)
+    plt.xticks(fontsize=TICK_SIZE)
 
     plt.tight_layout()
     plt.savefig(filename, format='pdf', dpi=300, bbox_inches='tight')  # Save the figure
