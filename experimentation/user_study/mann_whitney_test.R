b189c6f38b117176fdd2d41f1fbc2bec55a7e6c8
Laura Stampf <github@isadora.de>
2024-03-24 23:41:14 +0100

    Update: updated mann whitney test to be task dependent, now have 8 tests -> 2 dependent vars x 4 tasks

--- experimentation/user_study/mann_whitney_test.R
+++ experimentation/user_study/mann_whitney_test.R
@@ -1,8 +1,32 @@
 library(readxl)
+library(dplyr)
+
 data <- read_excel("raw-data.xlsx") # NOTE: ensure you are in the correct working directory (user_study)
 
-# Mann Whitney Test for Time(s)
-wilcox.test(`Time (s)` ~ `Experience level`, data = data, exact = FALSE)
+# Prepare a function to run the Mann-Whitney U test for a given task and dependent variable
+run_mann_whitney <- function(data, task, dependent_variable) {
+  # Filter data for the specific task
+  task_data <- data %>% filter(Task == task)
+  
+  # Convert dependent variable to a formula
+  formula <- as.formula(paste("`", dependent_variable, "` ~ `Experience level`", sep=""))
+  
+  # Perform the Mann-Whitney U test using wilcox.test
+  result <- wilcox.test(formula, data = task_data, exact = FALSE)
+  return(result)
+}
 
-# Mann Whitney Test for Number of clicks
-wilcox.test(`Number of clicks` ~ `Experience level`, data = data, exact = FALSE)
+# Initialize a list to store results
+results <- list()
+tasks <- unique(data$Task)
+dependent_variables <- c("Time (s)", "Number of clicks")
+
+# Run tests for each task and dependent variable
+for (task in tasks) {
+  for (dependent_variable in dependent_variables) {
+    results[[paste("Task", task, dependent_variable)]] <- run_mann_whitney(data, task, dependent_variable)
+  }
+}
+
+# Results
+results
